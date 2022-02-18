3154d7fcdb01829153a8f723759384072d233fc8
Muhammadalikie <muhammadalikie@stu.nit.ac.ir>
2022-02-18 20:50:15 +0330

    Update Tehran-Weather.py

--- Tehran-Weather.py
+++ Tehran-Weather.py
@@ -1,20 +1,9 @@
 import requests
 import json
 
-# url_api = "https://www.metaweather.com/api/location/search/?lattlong=35.6892,51.3890"
-# response = requests.get(url_api)
-
-# city_info = response.json()
-
-# f = open("C:/Users/markazi/Desktop/city_info.json", "w+")
-# json.dump(city_info, f)
-
 city_id = "2251945"
 url_api = "https://www.metaweather.com/api/location/"+ city_id +"/"
 response = requests.get(url_api)
 weather_info = response.json()
-
-# f = open("C:/Users/markazi/Desktop/weather_info.json", "a+")
-# json.dump(weather_info, f)
 
 print("----------\nTimeZone : %s\nWeather : %s\n----------" %(weather_info['timezone'],weather_info['consolidated_weather'][0]['weather_state_name']))
